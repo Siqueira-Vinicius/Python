bd1f7e2bcdfd5481724d32db387f51636bb4ad60
Siqueira-Vinicius <vinniesique@gmail.com>
2021-09-30 18:59:37 -0300

    descrevi alguns detalhes

--- exercicios/ex110.py
+++ exercicios/ex110.py
@@ -1,3 +1,4 @@
+# baixar moeda.py e armazenar na mesma pasta desse arquivo
 import moeda
 p = float(input(f'Digite um preço: {moeda.preço(formatar=True)}'))
 a = int(input('Qual o valor do acrescimo?: %'))
--- exercicios/ex115.py
+++ exercicios/ex115.py
@@ -1,3 +1,4 @@
+# baixar moeda.py e pessoas.txt e armazenar na mesma pasta que esse arquivo
 import home
 
 home.menu()
